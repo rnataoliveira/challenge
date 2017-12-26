b66a9579e83790f26b61828435f3f421e33c7e48
Renata Oliveira <renatabels@gmail.com>
2017-12-25 23:25:14 -0200

    Configurating Route

--- resolution/src/components/App.js
+++ resolution/src/components/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import Router from 'react-router';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+// import GitHubStars from './GitHubStars';
+import GetUserStars from './GetUserStars';
 import GitHubStars from './GitHubStars';
 
 export default class App extends React.Component {
     render() {
-        
         return ( 
-            <div>
-                <GitHubStars />
-            </div>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/" component={GetUserStars}/>
+                    <Route exact path="/stars" component={GitHubStars}/>
+                </Switch>
+            </BrowserRouter>
         );
     }
 }
