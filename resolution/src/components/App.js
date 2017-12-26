b05f79c16afbd75f6bbd2c635f8f6df175880253
Renata Oliveira <renatabels@gmail.com>
2017-12-26 00:18:08 -0200

    Changing the route path

--- resolution/src/components/App.js
+++ resolution/src/components/App.js
@@ -2,17 +2,16 @@ import React from 'react';
 import Router from 'react-router';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-// import GitHubStars from './GitHubStars';
-import GetUserStars from './GetUserStars';
-import GitHubStars from './GitHubStars';
+import UserImport from './UserImport';
+import Stars from './Stars';
 
 export default class App extends React.Component {
     render() {
         return ( 
             <BrowserRouter>
                 <Switch>
-                    <Route exact path="/" component={GetUserStars}/>
-                    <Route exact path="/stars" component={GitHubStars}/>
+                    <Route exact path="/" component={UserImport}/>
+                    <Route exact path="/:username/stars" component={Stars}/>
                 </Switch>
             </BrowserRouter>
         );
