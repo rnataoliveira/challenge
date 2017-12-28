c390c4add1d7299c2d534eac59fd6bb95fe1c6ea
Renata Oliveira <renatabels@gmail.com>
2017-12-28 00:51:14 -0200

    Adding link to brand, redirect to index

--- resolution/src/components/NavBar.js
+++ resolution/src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 import Search from './Search';
 
@@ -8,16 +9,15 @@ export default class NavBar extends React.Component {
             <div>
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
                     <div className="container">
-                        <a className="navbar-brand" href="#">GitHub Stars</a>
+                        <Link to="/">
+                            <a className="navbar-brand" href="#">GitHub Stars</a>
+                        </Link>
                         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav mr-auto">
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">Home</a>
-                                </li>
                             </ul>
                             <Search />
                         </div>
